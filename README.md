7b3fffe7446219bb749f444e11d54ebd4d5858d6
newt <76414005+afkNewt@users.noreply.github.com>
2026-01-16 18:12:21 -0800

    test if song can be skipped instead of using blacklist
    update README

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
-This is a project I use to skip ads on spotify. It keeps track of a list of all the names of ads on Spotify, and if ever it encounters one, restarts the program, which skips the ad.
-
-There is a small tui component to this that appears as
-
-```
-[0.6] Spotify Free
-[y/N]
-```
-
-where the number is a countdown to when it will next read the title of the Spotify app, the string afterwords being the current title of the Spotify app, and the prompt to be whether or not to add the name to the blacklist.
+Spawns the spotify app, then keeps checking to see if the current song can be skipped. If the song cannot be skipped, then spotify is restarted.
+This is meant to skip ads.
--- src/main.rs
+++ src/main.rs
@@ -1,127 +1,85 @@
-use std::{
-    fs::{self, OpenOptions},
-    io::Write,
-    sync::mpsc::{self, Receiver},
-    thread::{self},
-    time::{Duration, Instant},
-};
+use std::{process::Command, time::Duration};
+use zbus::{blocking::Connection, zvariant::OwnedValue};
 
-use console::{Key, Term};
-use spotify::Spotify;
+// busctl --user introspect org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2
+const DESTINATION: &str = "org.mpris.MediaPlayer2.spotify";
+const PATH: &str = "/org/mpris/MediaPlayer2";
 
-mod spotify;
+enum Method {
+    Ping,
+    Play,
+    Next,
+}
 
-const FILE_NAME: &str = "autoskip.txt";
-
-fn main() {
-    let term = Term::stdout();
-    let _ = term.hide_cursor();
-
-    let mut spotify = Spotify::spawn_new().unwrap();
-    let mut autoskip_file = OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open(FILE_NAME)
-        .unwrap();
-    let mut autoskip = fs::read_to_string(FILE_NAME)
-        .unwrap()
-        .lines()
-        .map(|str| str.to_string())
-        .collect::<Vec<String>>();
-
-    let mut now = Instant::now();
-    let rx = spawn_input();
-
-    let mut window_name = spotify.window_name().unwrap();
-    loop {
-        {
-            use ansi_control_codes::{
-                c0::CR,
-                control_sequences::{CUU, EL, EraseLine},
-            };
-            print!("{}{}{}", CUU(None), EL(Some(EraseLine::BeginToEnd)), CR);
+impl Method {
+    pub fn interface(&self) -> &str {
+        match self {
+            Method::Ping => "org.freedesktop.DBus.Peer",
+            Method::Play => "org.mpris.MediaPlayer2.Player",
+            Method::Next => "org.mpris.MediaPlayer2.Player",
         }
-
-        println!("[{:.1}] {window_name}", now.elapsed().as_secs_f32());
-
-        if let Ok(exit) = rx.try_recv() {
-            if exit {
-                let _ = term.show_cursor();
-                return;
-            }
-
-            autoskip.push(window_name.clone());
-            let _ = writeln!(autoskip_file, "{}", window_name);
+    }
+    pub fn name(&self) -> &str {
+        match self {
+            Method::Ping => "Ping",
+            Method::Play => "Play",
+            Method::Next => "Next",
         }
-
-        if now.elapsed() > Duration::from_secs(1) {
-            if spotify.exited() != Some(false) {
-                let _ = term.show_cursor();
-                return;
-            }
-
-            now = Instant::now();
-            window_name = spotify.window_name().unwrap();
-
-            if autoskip.contains(&window_name) {
-                println!("Skipping {}", window_name);
-                let _ = spotify.restart();
-            }
-        }
-
-        thread::sleep(Duration::from_millis(100));
     }
 }
 
-fn spawn_input() -> Receiver<bool> {
-    let (tx, rx) = mpsc::channel::<bool>();
-    let term = Term::stdout();
-    let mut choice = false;
+pub fn main() {
+    let connection = Connection::session().unwrap();
 
-    let prompt = |choice: bool| {
-        use ansi_control_codes::control_sequences::{EL, EraseLine};
+    let mut child = Command::new("spotify").spawn().unwrap();
+    wait_for_spotify_ready(&connection);
+    let _ = call_method(&connection, Method::Play);
 
-        print!(
-            "{}{}",
-            EL(Some(EraseLine::BeginToEnd)),
-            if choice { "[Y/n]" } else { "[y/N]" }
-        );
-    };
-
-    prompt(choice);
-
-    thread::spawn(move || {
-        loop {
-            let Ok(key) = term.read_key_raw() else {
-                continue;
-            };
-
-            match key {
-                Key::Char('y') | Key::Char('a') | Key::Char('h') | Key::ArrowLeft => {
-                    if !choice {
-                        choice = true;
-                        prompt(choice);
-                    }
-                }
-                Key::Char('n') | Key::Char('d') | Key::Char('l') | Key::ArrowRight => {
-                    if choice {
-                        choice = false;
-                        prompt(choice);
-                    }
-                }
-                Key::Enter => {
-                    if choice {
-                        let _ = tx.send(false);
-                    }
-                }
-                Key::CtrlC => {
-                    let _ = tx.send(true);
-                    return;
-                }
-                _ => {}
-            }
+    loop {
+        while get_boolean_property(&connection, "CanGoNext") {
+            std::thread::sleep(Duration::from_secs(1));
         }
-    });
 
-    return rx;
+        let _ = child.kill();
+        child = Command::new("spotify").spawn().unwrap();
+        wait_for_spotify_ready(&connection);
+        let _ = call_method(&connection, Method::Next);
+        let _ = call_method(&connection, Method::Play);
+    }
+}
+fn get_boolean_property(connection: &Connection, property: &str) -> bool {
+    return connection
+        .call_method(
+            Some(DESTINATION),
+            PATH,
+            Some("org.freedesktop.DBus.Properties"),
+            "Get",
+            &("org.mpris.MediaPlayer2.Player", property),
+        )
+        .unwrap()
+        .body()
+        .deserialize::<OwnedValue>()
+        .unwrap()
+        .try_into()
+        .unwrap();
+}
+
+fn call_method(connection: &Connection, method: Method) -> Result<zbus::Message, zbus::Error> {
+    return connection.call_method(
+        Some(DESTINATION),
+        PATH,
+        Some(method.interface()),
+        method.name(),
+        &(),
+    );
+}
+
+fn wait_for_spotify_ready(connection: &Connection) {
+    loop {
+        if call_method(connection, Method::Ping).is_ok() {
+            return;
+        }
+
+        std::thread::sleep(Duration::from_millis(50));
+    }
 }
